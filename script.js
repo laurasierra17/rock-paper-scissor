f05b37f086e0a44feb5900f78f09eadafa6830d4
JZ <jeffz98@berkeley.edu>
2022-06-23 11:40:15 -0700

    added computer choice

--- script.js
+++ script.js
@@ -1,2 +1,16 @@
 // Ask user to select between R, P, or S
 var userInput = prompt("Enter R, P, or S to play");
+
+// generates random number for computer choice
+var num = Math.floor(Math.random() * 3);
+var compChoice;
+
+// selects between R, P, S based off random number
+if(num === 0) {
+    compChoice = "R";
+} else if (num === 1) {
+    compChoice = "P";
+} else {
+    compChoice = "S";
+}
+console.log(compChoice);
